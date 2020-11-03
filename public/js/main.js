28144ee915a13fa82e8fdb192f4196457557e1df
AnassLachiri <lachirianass@gmail.com>
2020-11-03 11:24:27 +0100

    Changing the welcome page

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,23 @@
-new fullpage('#fullPage', {
-    autoScrolling: true,
-    navigation: true
-})
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        scrollingSpeed: 500,
+        easing: 'linear',
+        loopBottom: true,
+        loopTop: true,
+        css3: true,
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 1,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+    });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,19 +11,21 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/main.js') }}" defer></script>
+
+
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/fullpage.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/jquery.pagepiling.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/welcome_styles.css') }}" rel="stylesheet">
 
-    
+
 </head>
 <body>
 
@@ -39,31 +41,32 @@
             <a href="{{ route('register') }}">{{ __('Register') }}</a>
     </div>
 
-    <div id="fullPage">
-            <div class="section s1" style="background-image:  url('imgs/test1.jpg');">
-                <h1>Showcase</h1>
-                <div class="description">
-                    <p>Sell and showcase your paintings </p>
-                    <a href="{{ route('register') }}" ><button type="button">Get started</button></a>
-                </div>
-            </div>
-            <div class="section s2" style="background-image: url('imgs/section2.jpg');">
-                <h1>Discover</h1>
-                <div class="description">
-                    <p>Discover new paintings to buy</p>
-                    <a href="/explore" ><button type="button">Explore</button></a>
-                </div>
-            </div>
-            <div class="section s3" style="background-image: url('imgs/section3.jpg');">
-                <h1>Upvote</h1>
-                <div class="description">
-                    <p>Upvote your favorite paintings and artists</p>
-                </div>
+    <div id="pagepiling">
+        <div class="section s1" style="background-image:  url('imgs/test1.jpg');">
+            <h1>Showcase</h1>
+            <div class="description">
+                <p>Sell and showcase your paintings </p>
+                <a href="{{ route('register') }}" ><button type="button">Get started</button></a>
             </div>
         </div>
+        <div class="section s2" style="background-image: url('imgs/section2.jpg');">
+            <h1>Discover</h1>
+            <div class="description">
+                <p>Discover new paintings to buy</p>
+                <a href="/explore" ><button type="button">Explore</button></a>
+            </div>
+        </div>
+        <div class="section s3" style="background-image: url('imgs/section3.jpg');">
+            <h1>Upvote</h1>
+            <div class="description">
+                <p>Upvote your favorite paintings and artists</p>
+            </div>
+        </div>
+    </div>
 
-
-    <script src="js/fullpage.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/jquery.pagepiling.min.js"></script>
+    <script src="{{ asset('js/main.js') }}" defer></script>
 
 </body>
 </html>
