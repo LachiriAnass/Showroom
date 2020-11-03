82ab6aa9075bfe8dfe589be234dc984561411fd5
AnassLachiri <lachirianass@gmail.com>
2020-11-03 19:56:11 +0100

    Fixing the main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,14 +20,13 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/jquery.pagepiling.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/welcome_styles.css') }}" rel="stylesheet">
 
 
 </head>
-<body>
+<body style="height:100%">
 
     <div class="showroom-main-page-icon">
         <a href="{{ url('/') }}">
@@ -41,32 +40,26 @@
             <a href="{{ route('register') }}">{{ __('Register') }}</a>
     </div>
 
-    <div id="pagepiling">
-        <div class="section s1" style="background-image:  url('imgs/test1.jpg');">
-            <h1>Showcase</h1>
-            <div class="description">
-                <p>Sell and showcase your paintings </p>
-                <a href="{{ route('register') }}" ><button type="button">Get started</button></a>
-            </div>
-        </div>
-        <div class="section s2" style="background-image: url('imgs/section2.jpg');">
-            <h1>Discover</h1>
-            <div class="description">
-                <p>Discover new paintings to buy</p>
-                <a href="/explore" ><button type="button">Explore</button></a>
-            </div>
-        </div>
-        <div class="section s3" style="background-image: url('imgs/section3.jpg');">
-            <h1>Upvote</h1>
-            <div class="description">
-                <p>Upvote your favorite paintings and artists</p>
-            </div>
+    <div class="section s1" style="background-image:  url('imgs/test1.jpg');">
+        <h1>Showcase</h1>
+        <div class="description">
+            <p>Sell and showcase your paintings </p>
+            <a href="{{ route('register') }}" ><button type="button">Get started</button></a>
         </div>
     </div>
-
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/jquery.pagepiling.min.js"></script>
-    <script src="{{ asset('js/main.js') }}" defer></script>
+    <div class="section s2" style="background-image: url('imgs/section2.jpg');">
+        <h1>Discover</h1>
+        <div class="description">
+            <p>Discover new paintings to buy</p>
+            <a href="/explore" ><button type="button">Explore</button></a>
+        </div>
+    </div>
+    <div class="section s3" style="background-image: url('imgs/section3.jpg');">
+        <h1>Upvote</h1>
+        <div class="description">
+            <p>Upvote your favorite paintings and artists</p>
+        </div>
+    </div>
 
 </body>
 </html>
