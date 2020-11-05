a299e6f1998bbbe92c04a62dd49a44858a25c613
LachiriAnass <lachirianass5@gmail.com>
2020-11-05 18:45:29 +0100

    Didn't notice verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,13 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.login_and_register')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
+
+                     <div class="title">{{ __('Verify Your Email Address') }}</div>
+
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
